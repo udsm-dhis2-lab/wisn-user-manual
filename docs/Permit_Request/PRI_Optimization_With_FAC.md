c836e0f0d4b889f9a5ee344491f9fbe583f48411
Oswaldgerald <moswaldgerald@gmail.com>
2021-11-16 18:15:12 +0300

    added Permit Requests files with manual contents

---
+++ docs/Permit_Request/PRI_Optimization_With_FAC.md
@@ -0,0 +1,21 @@
+---
+id: PRI Optimization with Full Automated Cadres (FAC)
+title: PRI Optimization with Full Automated Cadres (FAC)
+---
+
+The initial optimization process allows the LGA users to apply for permit allocation to all fully automated cadres as per the PRI. The selected cadres for allocation will be determined by the PRI, budget and supply constraints.
+
+![img alt](/img/PRI_FAC.png)
+         List of cadres arranged based on PRI
+        
+ the user can filter the list to view only the newly assigned list by pressing the “PRI filter” button (see Figure below).
+
+ ![img alt](/img/PRI_FAC2.png)
+         Applying PRI filter
+
+ Choosing the “New assignment” option will display only facility cadres that have been assigned with permits (as in Figure below).
+
+ ![img alt](/img/PRI_FAC2.png)
+         Cadres that have been assigned with permits
+
+On the top right corner above the New Assignment list, users can view the budget spent and any remaining amount. Clicking the “Salaries” button will display information about all FAC monthly salaries.
--- sidebars.js
+++ sidebars.js
@@ -47,6 +47,32 @@ module.exports = {
         label: "User Module",
         items: ["users/users"],
       },
+      {
+        type: "category",
+        label: "Analytics",
+        items: ["Analytics/Analytics"],
+      },
+      {
+        type: "category",
+        label: "Redistribution",
+        items: ["Redistribution/home","Redistribution/introduction", 
+        "Redistribution/Surplus-Identification", "Redistribution/Verification", 
+        "Redistribution/Customization-Confirmation"],
+      },
+      {
+        type: "category",
+        label: "LGA-Redistribution",
+        items: ["LGA-Redistribution/LGA_Redistribution_introduction","LGA-Redistribution/LGA_Redistribution_home",
+        "LGA-Redistribution/LGA_Surplus-Identification", "LGA-Redistribution/LGA_Recommended_Redistribution",
+      "LGA-Redistribution/LGA_Customization_And_Confirmation", "LGA-Redistribution/LGA_Verification"],
+      },
+      {
+        type: "category",
+        label: "Permit_Request",
+        items: ["Permit_Request/Enter_Budget_Allocation","Permit_Request/Cadre_Ceiling",
+         "Permit_Request/New_and_Upgraded_Facilities", "Permit_Request/PRI_Optimization_With_FAC", 
+         "Permit_Request/Permit_UAC","Permit_Request/Prioritized_Allocation"],
+      },
     ],
   },
 };
